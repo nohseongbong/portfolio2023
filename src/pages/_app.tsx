da69d01a6f281f5e45b5bdd14a4afe4d2bdcd898
nohseongbong <shtjdqhd@naver.com>
2023-10-03 18:13:14 +0900

    feat: queryProvider 작업

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,17 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  const queryClient = new QueryClient();
+
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="layout_component">
+        <Component {...pageProps} />
+      </div>
+      <ReactQueryDevtools initialIsOpen={process.env.NODE_ENV === "development"} />
+    </QueryClientProvider>
+  );
 }
