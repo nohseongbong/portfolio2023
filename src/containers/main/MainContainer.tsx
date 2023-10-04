8996dc613f85861705e4e7ce4970b5a01c94cca4
nohseongbong <shtjdqhd@naver.com>
2023-10-04 14:55:41 +0900

    feat: main page 작업중

---
+++ src/containers/main/MainContainer.tsx
@@ -0,0 +1,5 @@
+const Maincontainer = () => {
+  return <div>메인 페이지</div>;
+};
+
+export default Maincontainer;
---
+++ src/pages/main/index.tsx
@@ -0,0 +1,5 @@
+import Maincontainer from "@/containers/main/MainContainer";
+
+export default function LoginPage() {
+  return <Maincontainer />;
+}
