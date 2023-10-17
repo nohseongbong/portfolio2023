7bff58965179615b4dd855917e0926d6784e3100
nohseongbong <shtjdqhd@naver.com>
2023-10-17 19:54:10 +0900

    design: header 수정

--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,5 +1,29 @@
+import styles from "@/styles/component/header.module.css";
+
+import Link from "next/link";
+
 const Header = () => {
-  return <div>헤더</div>;
+  return (
+    <div className={styles.header_wrap}>
+      <ul className={styles.nav_list}>
+        <li className={styles.nav_list_item}>
+          <Link legacyBehavior href="/">
+            <a className={styles.nav_list_item_text}>Works</a>
+          </Link>
+        </li>
+        <li className={styles.nav_list_item}>
+          <Link legacyBehavior href="/about">
+            <a className={styles.nav_list_item_text}>About</a>
+          </Link>
+        </li>
+        <li className={styles.nav_list_item}>
+          <Link legacyBehavior href="/contact">
+            <a className={styles.nav_list_item_text}>Contact</a>
+          </Link>
+        </li>
+      </ul>
+    </div>
+  );
 };
 
 export default Header;
