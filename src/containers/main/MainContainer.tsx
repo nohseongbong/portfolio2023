41611c9805d82221f444dc4b77ac9b985843b905
nohseongbong <shtjdqhd@naver.com>
2023-10-17 19:55:49 +0900

    refactor: maincontainer 수정

--- src/containers/main/MainContainer.tsx
+++ src/containers/main/MainContainer.tsx
@@ -1,10 +1,12 @@
 import SwiperMain from "@/components/swiper/SwiperMain";
 import { useTodos } from "@/hooks/useProjects";
+import styles from "@/styles/page/main.module.css";
 
 const Maincontainer = () => {
   const { projects } = useTodos();
+  console.log(projects, ":projects");
   return (
-    <div className="w-screen h-full bg-pink">
+    <div className={styles.main_container}>
       <SwiperMain projects={projects} />
     </div>
   );
