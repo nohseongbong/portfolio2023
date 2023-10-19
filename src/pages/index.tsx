47322223c25ec665a523728a84cd1326bf647288
nohseongbong <shtjdqhd@naver.com>
2023-10-19 15:05:48 +0900

    design : 메인 페이지 수정

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,11 @@
-import { Inter } from "next/font/google";
-
-const inter = Inter({ subsets: ["latin"] });
+import Header from "@/components/header/header";
+import Maincontainer from "@/containers/main/MainContainer";
 
 export default function Home() {
-  return <main className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}></main>;
+  return (
+    <div className="h-screen">
+      <Header />
+      <Maincontainer />
+    </div>
+  );
 }
